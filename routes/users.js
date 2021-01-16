ca2cd37eea09f4536feca8a6354bb7a1a0a747b5
him7797 <45537889+him7797@users.noreply.github.com>
2021-01-16 23:06:34 +0530

    Way To Deploy

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const Router = express.Router();
 const Validation=require('../validations/user');
-// const forgotPassword=require('../models/forgotPassword');
 const User=require('../models/user');
 const Post=require('../models/post');
-// const msg91=require('../helper/otp');
 const bcrypt=require('bcrypt');
 const Category=require('../models/category');
 const multer = require('multer');
@@ -180,9 +178,9 @@ Router.post('/signUp',asyncMiddleware(async(req,res)=>{
 //Login Route
 Router.post('/logIn',asyncMiddleware(async(req,res)=>{
   let email=req.body.email;
-  if (email.length<5) return res.status(401).json({
+  if (email.length<2) return res.status(401).json({
     status: "Failed",
-    message: "Length is less than 10"
+    message: "Length is less than 2"
 });
 let checkUser=await User.findOne({email:email});
 if(!checkUser) return res.status(401).json({
@@ -309,7 +307,7 @@ Router.get('/:id',auth,asyncMiddleware(async(req,res)=>{
         userInfo:UserInfo,
         cat:catInfo,
         currentUser:currentUser
-    })
+    });
 }));
 
 Router.get('/editProfile/:id',auth,asyncMiddleware(async(req,res)=>{
@@ -328,107 +326,7 @@ Router.get('/editProfile/:id',auth,asyncMiddleware(async(req,res)=>{
 
 }));
 
-// Router.post('/forgotPas/byPhone',asyncMiddleware(async(req,res)=>{
-//       let phone=req.body.phone;
-//       if(phone.length<10 && phone.length>10)
-//       {
-//         return res.status(401).json({
-//           status: "Failed",
-//           message: "Phone number invalid"
-//         });
-//       }
-//       // let userInfo=await User.findOne({phone:phone});
-//
-//         let code=Math.floor(1000 + Math.random() * 9000);
-//         // const newForPas=new forgotPassword({
-//         //   user:userInfo._id,
-//         //   phone:userInfo.phone,
-//         //   code:code
-//         //
-//         // await newForPas.save();
-//         // await forgotPassword.updateOne({_id:newForPas._id},{$set:{status:true}});
-//         let Send=await msg91.sendOTP(code,phone);
-//         if(Send.type!="success")
-//         {
-//           return res.status(401).json({
-//             status: "Failed",
-//             message: "Code Not Sent",
-//               body:Send
-//
-//
-//         });
-//         }
-//         else{
-//           return res.status(200).json({
-//             status: "Success",
-//             message: "Code Sent",
-//
-//             body:Send
-//         });
-//         }
-//       }));
-//
-// Router.post('/otp',asyncMiddleware(async(req,res)=>{
-//     let code=req.body.code;
-//     let phone=req.body.phone;
-//     let doc=await forgotPassword.findOne({phone:phone,code:code,status:true});
-//     if(doc)
-//     {
-//       await forgotPassword.updateOne({phone:phone,status:true},{$set:{status:false}});
-//       return res.status(200).json({
-//         status: "Success",
-//         message: "Otp Verified!"
-//     });
-//     }
-//     else{
-//       return res.status(401).json({
-//         status: "Failed",
-//         message: "Otp Not Verified!"
-//     });
-//     }
-//
-// }));
-//
-//
-// Router.post('/change-Password-otp',asyncMiddleware(async(req,res)=>{
-//       let newPass=req.body.newPass;
-//       let phone=req.body.phone;
-//       let code=req.body.code;
-//       let checPass=await Validation.validatePassword(newPass);
-//         if(checPass==false)
-//         {
-//           res.status(401).json({status:'Failed',message:'Invalid Password Entry! Retry'});
-//         }
-//       let doc=await forgotPassword.findOne({code:code,status:false,phone:phone});
-//       if(doc)
-//       {
-//         const salt=await bcrypt.genSalt(10);
-//         newPass=await bcrypt.hash(newPass,salt);
-//         let result=await User.updateOne({phone:phone},{$set:{password:newPass}});
-//         return res.status(200).json({
-//           status: "Success",
-//           message: "Password Successfully Changed!"
-//       });
-//       }
-//       else{
-//         return res.status(401).json({
-//           status: "Failed",
-//           message: "OTP not verified!"
-//       });
-//       }
-// }));
 
-
-
-
-// Router.post('/update/userInfo/:id',async(req,res)=>{
-//      let _id=re.params._id;
-//      let userInfo=await User.findById(_id);
-//      if(userInfo)
-//      {
-
-//      }
-// });
 
 
 module.exports=Router;
