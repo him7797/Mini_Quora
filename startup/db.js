4e0b2fbb5f86842db7d5c709a193ae6ee885bb3d
him7797 <45537889+him7797@users.noreply.github.com>
2021-01-17 22:01:14 +0530

    Remove node_modules

--- startup/db.js
+++ startup/db.js
@@ -5,6 +5,6 @@ require('winston-mongodb');
 
 
 module.exports=function(){
-    mongoose.connect('mongodb+srv://Himanshu:HIM101hi@@miniquora.ztdid.mongodb.net/miniquora?retryWrites=true&w=majority')
+    mongoose.connect('mongodb+srv://Himanshu:HIM101hi@miniquora.ztdid.mongodb.net/miniquora?retryWrites=true&w=majority',{useNewUrlParser: true})
         .then(() => winston.info('Connected to MongoDB...'));
 }
